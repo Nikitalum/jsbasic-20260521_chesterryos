087180407f46ef534f0e630d4a152825ee95a750
Nikitalum <chesterryos@gmail.com>
2026-05-31 11:21:45 +0900

    Решены 3,4,5 задачи 1 модуля

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,24 @@
 function checkSpam(str) {
-  // ваш код...
+  let spam = '1xBet'
+  let spam2 = 'XXX'
+  let exist1 = 0
+  let exist2 = 0
+  let j = 0
+  let k = 0
+  for (i = 0; i < str.length; i++){
+      if (str.toLowerCase()[i] == spam.toLowerCase()[j]){
+          exist1 += 1
+          j += 1
+      }
+      if (str.toLowerCase()[i] == spam2.toLowerCase()[k]){
+          exist2 += 1
+          k += 1
+      }
+  }
+  if (exist1 == spam.length || exist2 == spam2.length){
+      return true
+  }
+  else{
+      return false
+  }
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,13 @@
 function truncate(str, maxlength) {
-  // ваш код...
+    if (str.length > maxlength) {
+        let i
+        str_fin = str[0]
+        for(i = 1; i < maxlength - 1; i++) {
+            str_fin = str_fin + str[i]
+        }
+    return (str_fin + '…');
+    }
+    else {
+    return str;
+    }
 }
