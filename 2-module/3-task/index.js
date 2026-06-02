2bcc9410f68ecfe717be66a46accbea26deb4f46
Nikitalum <chesterryos@gmail.com>
2026-06-03 08:24:20 +0900

    Решены задачи 2 модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,7 @@
 let calculator = {
-  // ваш код
+    read: function(a,b){el1 = a, el2 = b},
+    sum: function(){return el1 + el2},
+    mul: function(){return el1 * el2},
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
